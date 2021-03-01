08cf0cbd260666c61247d21a743bd9fd7519e306
charliebrown364 <364charliebrown@gmail.com>
2021-03-01 05:51:54 +0000

    Made hash_table a class for assignment 61

--- hash_table.py
+++ hash_table.py
@@ -1,34 +1,53 @@
-buckets = [[], [], [], [], []]
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+class HashTable():
 
-def hash_function(string):
-    alphabet_dict = {alphabet[i]:i for i in range(len(alphabet))}
-    total = 0
-    for char in string:
-        total += alphabet.index(char)
-    return total % 5
+    def __init__(self, num_buckets):
+        self.num_buckets = num_buckets
+        self.buckets = [[] for _ in range(self.num_buckets)]
+    
+    def hash_function(self, string):
+        
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        alphabet_dict = {alphabet[i]:i for i in range(len(alphabet))}
+        
+        total = 0
+        for char in string:
+            total += alphabet.index(char)
+        
+        return total % self.num_buckets
+    
+    def insert(self, key, value):
+        pair = (key, value)
+        bucket_index = self.hash_function(key)
+        self.buckets[bucket_index].append(pair)
+    
+    def find(self, key):
+        bucket_index = self.hash_function(key)
+        for bucket in self.buckets:
+            for pair in bucket:
+                if pair[0] == key:
+                    return pair[1]
 
-def insert(buckets, key, value):
-    pair = (key, value)
-    bucket_index = hash_function(key)
-    buckets[bucket_index].append(pair)
+print("Testing HashTable")
 
-def find(buckets, key):
-    bucket_index = hash_function(key)
-    for bucket in buckets:
-        for pair in bucket:
-            if pair[0] == key:
-                return pair[1]
+ht = HashTable(num_buckets = 3)
+assert ht.buckets == [[], [], []]
+assert ht.hash_function('cabbage') == 2 # (because 2 + 0 + 1 + 1 + 0 + 6 + 4 mod 3 = 14 mod 3 = 2)
 
-for i, char in enumerate(alphabet):
-    key = 'someletters' + char
-    value = [i, i**2, i**3]
-    insert(buckets, key, value)
+ht.insert('cabbage', 5)
+assert ht.buckets == [[], [], [('cabbage',5)]]
 
-for i, char in enumerate(alphabet):
-    key = 'someletters' + char
-    output_value = find(buckets, key)
-    desired_value = [i, i**2, i**3]
-    assert output_value == desired_value
+ht.insert('cab', 20)
+assert ht.buckets == [[('cab', 20)], [], [('cabbage',5)]]
+
+ht.insert('c', 17)
+assert ht.buckets == [[('cab', 20)], [], [('cabbage',5), ('c',17)]]
+
+ht.insert('ac', 21)
+assert ht.buckets == [[('cab', 20)], [], [('cabbage',5), ('c',17), ('ac', 21)]]
+
+assert ht.find('cabbage') == 5
+assert ht.find('cab') == 20
+assert ht.find('c') == 17
+assert ht.find('ac') == 21
 
 print("passed all")
